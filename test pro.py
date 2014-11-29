ac572834a834f984f6bbdda4a03484194e48fd68
Mongkol Umaong <s7070093@kmitl.ac.th>
2014-11-29 20:45:17 +0700

    add
    add geometry and Entry

--- test pro.py
+++ test pro.py
@@ -1,11 +1,19 @@
 from Tkinter import *
 import random
+""" test = like_a_boss pro kak noob inwza"""
 def random_choice():
-    t = ['a', 'b', 'c', 'd']
-    print(random.choice(t))
+    list_t = t.get()
+    listt = list_t.split(' ')
+    ress = random.choice(listt)
+    res = Label(root, text="here : %s" % ress)
+    res.pack()
 root = Tk()
-title_t = Label(root, text="Get Random")
+t = StringVar()
+root.geometry('400x200')
+title_t = Label(root, text="input each choice with ' ' ")
 title_t.pack()
+ran_choice = Entry(root, textvariable=t)
+ran_choice.pack()
 random_b = Button(root, text="random", command=random_choice)
 random_b.pack()                  
 root.mainloop()
