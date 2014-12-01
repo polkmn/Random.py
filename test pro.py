3e8850bf4361460938e98bdadeabc3690fd29f66
Mongkol Umaong <s7070093@kmitl.ac.th>
2014-12-01 19:12:02 +0700

    beta
    grid to pack
    add Clear button, Quit, refresh result

--- test pro.py
+++ test pro.py
@@ -2,14 +2,23 @@ from Tkinter import *
 import random
 """ test = like_a_boss pro kak noob inwza"""
 def random_choice():
-    list_t = t.get()
-    listt = list_t.split(' ')
-    ress = random.choice(listt)
-    res = Label(root, text="here : %s" % ress).grid(row=3, column=1)
+        list_t = t.get()
+        listt = list_t.split()
+        ress = random.choice(listt)
+        res.config(text=">>> %s" %ress)
+        
+def clear_text():
+    ran_choice.delete(0, END)
+    
 root = Tk()
 t = StringVar()
-root.geometry('400x200')
-title_t = Label(root, text="input each choice with ' ' ").grid(row=0, column=0)
-ran_choice = Entry(root, textvariable=t).grid(row=0, column=1)
-random_b = Button(root, text="random", command=random_choice).grid(row=0, column=2)            
+#root.geometry('400x200')
+title_t = Label(root, text="input box").pack()
+ran_choice = Entry(root, textvariable=t)
+ran_choice.pack()
+random_b = Button(root, text="random", command=random_choice).pack()
+clear_b = Button(root, text="Clear", command=clear_text).pack()
+res = Label(root)
+res.pack()
+Button(root, text="Quit", command=root.destroy).pack()
 root.mainloop()
