e3e39d02f29f2217a6050df8237aeece454991e5
Mongkol Umaong <s7070093@kmitl.ac.th>
2014-12-06 20:31:47 +0700

    test new window
    new window for output

--- test pro.py
+++ test pro.py
@@ -4,21 +4,34 @@ import random
 def random_choice():
         list_t = t.get()
         listt = list_t.split()
-        ress = random.choice(listt)
+        if listt == []:
+                ress = ''
+        else:
+                ress = random.choice(listt)
+                get_new_win(ress)
+
+def get_new_win(ress):
+
+        newwin = Toplevel(root)
+        res = Label(newwin)
         res.config(text=">>> %s" %ress)
+        res.pack()
+        def quit_win():
+                newwin.destroy()
+        Button(newwin,text='Quit',command=quit_win).pack()
+        newwin.after(5000, func=newwin.destroy)                
+        
         
 def clear_text():
     ran_choice.delete(0, END)
     
 root = Tk()
 t = StringVar()
-#root.geometry('400x200')
+root.geometry('400x200')
 title_t = Label(root, text="input box").pack()
 ran_choice = Entry(root, textvariable=t)
 ran_choice.pack()
 random_b = Button(root, text="random", command=random_choice).pack()
 clear_b = Button(root, text="Clear", command=clear_text).pack()
-res = Label(root)
-res.pack()
 Button(root, text="Quit", command=root.destroy).pack()
 root.mainloop()
