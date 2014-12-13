594ee6fdc1f307207af82d6e1e32394aa5a6d5fc
wertanwee <s7070057@kmitl.ac.th>
2014-12-14 02:04:13 +0700

    Fix Project
    Fix position and add option in menubar

--- Project.py
+++ Project.py
@@ -35,13 +35,21 @@ class Program:
         self.button.config(height = 1, width = 9)
         #MenuBar
         self.menuBar = Menu(main)
-        self.aboutMenu = Menu(self.menuBar, tearoff=0)
-        self.aboutMenu.add_command(label="Developer", command=self.about_window)
-        self.aboutMenu.add_command(label="EXIT", command=self.exit_window)
-        self.menuBar.add_cascade(label="Help", menu=self.aboutMenu)
+        self.fileMenu = Menu(self.menuBar, tearoff=0)
+        self.menuBar.add_cascade(label="File", menu=self.fileMenu)
+        self.fileMenu.add_command(label="EXIT", command=self.exit_window)
+
+        self.optionMenu = Menu(self.menuBar, tearoff=0)
+        self.menuBar.add_cascade(label='Option', menu=self.optionMenu)
+        self.optionMenu.add_command(label='Number', command=self.num_random)
+        self.optionMenu.add_command(label='Update..Soon', command='null')
+
+        self.HelpMenu = Menu(self.menuBar, tearoff=0)
+        self.menuBar.add_cascade(label="Help", menu=self.HelpMenu)
+        self.HelpMenu.add_command(label="Developer", command=self.author_window)
         main.config(menu=self.menuBar)
 
-    def about_window(self):
+    def author_window(self):
         #Author Menu Window
         def thatOk():
           about.destroy()
@@ -54,6 +62,30 @@ class Program:
         msg.pack()
         button = Button(about, text="Accept", command=thatOk)
         button.place(x = 155, y = 75)
+
+    def num_random(self):
+        #Random_number
+        self.num_ran = Toplevel(start.main, bg='#000000')
+        self.num_ran.geometry('300x300')
+        self.num_ran.resizable(width=FALSE, height=FALSE)
+        self.num_ran.title("Random Number")
+        #entries
+        self.fillnum1 = self.numfill(values.fillnum1)
+        self.fillnum1.config(width=5, font = ('', 14))
+        self.fillnum2 = self.numfill(values.fillnum2)
+        self.fillnum2.config(width=5, font = ('', 14))
+        self.resnum = self.numfill(values.resnum)
+        self.resnum.config(width=10, state='readonly', readonlybackground='#FFFFFF', font = ('', 16))
+        #button
+        self.button = Button(self.num_ran, text = 'RANDOM!', command = self.rannum)
+        self.button.config(height = 1, width = 9)
+        #pack
+        self.fillnum1.place(x=33, y=125)
+        self.button.place(x=112, y=125)
+        self.fillnum2.place(x=203, y=125)
+        self.resnum.place(x=85, y=200)
+        
+        
     
     def exit_window(self):
         self.quit = exit()
@@ -62,6 +94,10 @@ class Program:
     def textfill(self, var):
         #create entry
         return Entry(start.main, textvariable = var, bg = '#FFFFFF')
+
+    def numfill(self, var):
+        #create entry for numran
+        return Entry(self.num_ran, textvariable = var, bg = '#FFFFFF')
 
     def textlabel(self, main, your_text, color = '#FFFFFF'):
         #Create Label
@@ -75,8 +111,22 @@ class Program:
             res = random.choice(fill1.split(','))
             values.res.set('%s' % res)
 
+    def rannum(self):
+        fill1 = values.fillnum1.get()
+        fill2 = values.fillnum2.get()
+        data = range(fill1, fill2 + 1)
+        if fill1 == '' and fill2 == '':
+            Msgbox.showerror('Error!', 'Fill number in both of two entries')
+        else:
+            resn = random.choice(data)
+            values.resnum.set('%s' % resn)
+        
+
     def call_random(self, event):
         self.random()
+
+    def call_rannum(self, event):
+        self.rannum()
 
     def guipack(self):
         #place position
@@ -90,6 +140,10 @@ class Values:
         def __init__(self):
             self.filltext = StringVar()
             self.res = StringVar()
+            self.fillnum1 = IntVar()
+            self.fillnum2 = IntVar()
+            self.resnum = StringVar()
+            
 
 
 start = Start()
